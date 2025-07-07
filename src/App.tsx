56d8751c15967f75ec886b1804073a22b8c1ec33
Andres Layedra <alayedra@espol.edu.ec>
2025-07-07 13:54:56 -0500

    Guia 12 y  13

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
+import IndicatorUI from './components/IndicatorUI';
+import DataFetcher from './functions/DataFetcher';
 
+
+type DataFetcherOutput = {
+   loading: boolean;
+   error: string | null;
+   data: {
+      current: {
+         temperature_2m: number;
+         apparent_temperature: number;
+         wind_speed_10m: number;
+         relative_humidity_2m: number;
+      };
+      current_units: {
+         temperature_2m: string;
+         apparent_temperature: string;
+         wind_speed_10m: string;
+         relative_humidity_2m: string;
+      };
+   } | null;
+};
 
 function App() {
+   const dataFetcherOutput = DataFetcher() as DataFetcherOutput;
    return (
       <>
          <div className="App" style={{ textAlign: 'center', margin: '10px' }}>
@@ -20,10 +43,65 @@ function App() {
             </Grid>
 
             {/* Selector */}
-            <Grid  size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+            <Grid  size={{ xs: 12, md: 3 }}>Elemento: Selector
+               <SelectorUI/>
+            </Grid>
 
             {/* Indicadores */}
-            <Grid  size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+            <Grid  size={{ xs: 12, md: 9 }}>
+               <Grid size={{ xs: 12, md: 3 }}>
+                  <IndicatorUI title='Temperatura (2m)' description='XX°C' />
+               </Grid>
+
+               <Grid size={{ xs: 12, md: 3 }}>
+                  <IndicatorUI title='Temperatura aparente' description='YY°C' />
+               </Grid>
+
+               <Grid size={{ xs: 12, md: 3 }}>
+                  <IndicatorUI title='Velocidad del viento' description='ZZkm/h' />
+               </Grid>
+
+               <Grid size={{ xs: 12, md: 3 }}>
+                  <IndicatorUI title='Humedad relativa' description='NN%' />
+               </Grid>
+
+               {/* Renderizado condicional de los datos obtenidos */}
+
+                 {dataFetcherOutput.loading && <p>Cargando datos...</p>}
+                 {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
+                 {dataFetcherOutput.data && (
+                 <>
+
+                     {/* Indicadores con datos obtenidos */}
+
+                     <Grid size={{ xs: 12, md: 3 }} >
+                         <IndicatorUI
+                             title='Temperatura (2m)'
+                             description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Temperatura aparente'
+                             description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Velocidad del viento'
+                             description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Humedad relativa'
+                             description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
+                     </Grid>
+
+                 </>
+                 )}
+
+            </Grid>
 
             {/* Gráfico */}
             <Grid sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
---
+++ src/types/DashboardTypes.ts
@@ -0,0 +1,14 @@
+export type OpenMeteoResponse = {
+  current: any;
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+  };
+};
